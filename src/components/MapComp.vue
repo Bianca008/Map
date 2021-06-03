71a0623914064e16c7efe9d6cd25a6c8b0ec399a
Bianca <lixandrubianca01@yahoo.ro>
2021-06-03 08:46:12 +0300

    Created a component for map

---
+++ src/components/MapComp.vue
@@ -0,0 +1,25 @@
+<template>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+  </head>
+  <body>
+
+    <div id="app" style="height: 400px">
+      <v-map :zoom=13 :center="[47.413220, -1.219482]">
+        <v-tilelayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></v-tilelayer>
+        <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+      </v-map>
+    </div>
+  </body>
+</html>
+</template>
+
+<script>
+export default {
+  name: 'MapComp',
+  props: {
+    msg: String
+  }
+}
+</script>
