fcd5292f77089b0adec04df7a242e34757fe1a54
Bianca <lixandrubianca01@yahoo.ro>
2021-06-04 15:11:47 +0300

    Added table

--- src/components/TableComp.vue
+++ src/components/TableComp.vue
@@ -1,32 +1,23 @@
 <template>
     <div>
-        <div v-for="data in myJson " :key="data.name">{{data}}</div>
-        <!-- <template>
-        <v-data-table : items="{myJson}">
-        </v-data-table>
-        </template> -->
-        <!-- <vue-table-dynamic :params="myJson" ref="table"></vue-table-dynamic> -->
-        <!-- <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Latitude</th>
-                    <th>Long</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="data in myJson " :key="data.name">
-                    <td>{{data.name}}</td>
-                    <td>{{data.geometry.coordinates[0]}}</td>
-                    <td>Long</td>
-                </tr>
-            </tbody>
-        </table> -->
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+            </tr>
+            <tr v-for="data in myJson.features" :key="data.params">
+                <td>{{data.properties.Id}}</td>
+                <td>{{data.geometry.coordinates[0]}}</td>
+                <td>{{data.geometry.coordinates[1]}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-      import json from '../response.json'
+import json from '../response.json'
+
       export default{
           data(){
               return{
@@ -35,3 +26,18 @@
           }
       }
 </script>
+
+<style scoped>
+
+table{
+    width: 100%;
+    background-color: aqua;
+    margin-top: 18%;
+}
+
+td{
+    text-align: center;
+    background-color: aquamarine;
+}
+
+</style>
